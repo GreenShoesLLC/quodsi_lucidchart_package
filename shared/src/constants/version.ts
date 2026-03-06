2ab885d2553194794f53fee8b70b07f347766ad0
Dapp3rDanH <5427730+Dapp3rDanH@users.noreply.github.com>
2026-03-06 07:44:59 -0500

    updated app version

--- shared/src/constants/version.ts
+++ shared/src/constants/version.ts
@@ -2,7 +2,7 @@
  * Current version of Quodsi
  * Should be updated to match package.json version when making releases
  */
-export const QUODSI_VERSION = "2026.02.28";
+export const QUODSI_VERSION = "2026.03.01";
 
 /**
  * Current version of the Quodsim simulation engine
