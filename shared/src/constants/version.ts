9fd2fd9e0eafaabf19b80df0bcd5d18d881ede24
Dapp3rDanH <5427730+Dapp3rDanH@users.noreply.github.com>
2026-04-23 15:32:22 -0400

    updated required simulation version

--- shared/src/constants/version.ts
+++ shared/src/constants/version.ts
@@ -8,7 +8,7 @@ export const QUODSI_VERSION = "2026.04.16";
  * Current version of the Quodsim simulation engine
  * This tracks the Python simulation runner version used by Azure Batch
  */
-export const QUODSIM_VERSION = "2026.04.04";
+export const QUODSIM_VERSION = "2026.04.22";
 
 /**
  * Version information broken down into components
